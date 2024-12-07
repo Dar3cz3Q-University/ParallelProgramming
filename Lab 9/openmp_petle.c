62d25dca96a13924cecc29436f2647609da084fc
Dariusz Homa <45432804+Dar3cz3Q@users.noreply.github.com>
2024-12-07 13:25:49 +0100

    Lab 9 (#9)
    * feat: first task done
    
    * Niby git
    
    * fix: teraz jest git
    
    * fix: remove unnecessary function

---
+++ Lab 9/openmp_petle.c
@@ -0,0 +1,134 @@
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WYMIAR 10
+
+int main() {
+    double a[WYMIAR][WYMIAR];
+
+    for (int i = 0; i < WYMIAR; i++)
+        for (int j = 0; j < WYMIAR; j++)
+            a[i][j] = 1.02 * i + 1.01 * j;
+
+    // podwójna pętla - sekwencyjnie
+    double suma = 0.0;
+    for (int i = 0; i < WYMIAR; i++) {
+        for (int j = 0; j < WYMIAR; j++) {
+            suma += a[i][j];
+        }
+    }
+
+    printf("Suma wyrazów tablicy: %lf\n", suma);
+
+    //
+    // Sumowanie po wierszach
+    //
+
+    double suma_parallel = 0.0;
+#pragma omp parallel for default(none) reduction(+ : suma_parallel) shared(a) schedule(static, 2) ordered
+    for (int i = 0; i < WYMIAR; ++i) {
+        for (int j = 0; j < WYMIAR; ++j) {
+            suma_parallel += a[i][j];
+        }
+#pragma omp ordered
+        printf("i: %d\tthread_id: %d\n", i, omp_get_thread_num());
+    }
+
+    printf("Suma wyrazów tablicy równolegle (wiersze): %lf\n", suma_parallel);
+
+    //
+    // Sumowanie po kolumnach
+    //
+
+    suma_parallel = 0.0;
+#pragma omp parallel for default(none) reduction(+ : suma_parallel) shared(a) schedule(dynamic)
+    for (int j = 0; j < WYMIAR; ++j) {
+        for (int i = 0; i < WYMIAR; ++i) {
+            suma_parallel += a[i][j];
+        }
+        printf("i: %d\tthread_id: %d\n", j, omp_get_thread_num());
+    }
+
+    printf("Suma wyrazów tablicy równolegle (kolumny): %lf\n", suma_parallel);
+
+    //
+    // Sumowanie po kolumnach z sekcją krytyczną
+    //
+
+    suma_parallel = 0.0;
+#pragma omp parallel for default(none) shared(suma_parallel, a) ordered
+    for (int j = 0; j < WYMIAR; ++j) {
+        double local_sum = 0.0;
+        for (int i = 0; i < WYMIAR; ++i) {
+            local_sum += a[i][j];
+        }
+#pragma omp critical
+        {
+            suma_parallel += local_sum;
+        }
+#pragma omp ordered
+        printf("j: %d\tthread_id: %d\n", j, omp_get_thread_num());
+    }
+
+    printf("Suma wyrazów tablicy równolegle (kolumny z sekcją krytyczną): %lf\n", suma_parallel);
+
+    //
+    // Dekompozycja 2D
+    //
+
+    omp_set_nested(1);
+    double tab[3][2];
+
+#pragma omp parallel for schedule(static, 2) num_threads(3) default(none) shared(a, tab) ordered
+    for (int i = 0; i < WYMIAR; ++i) {
+        int thread_id_outer = omp_get_thread_num();
+#pragma omp parallel for schedule(static, 2) num_threads(2) default(none) shared(a, tab, i) firstprivate(thread_id_outer) ordered
+        for (int j = 0; j < WYMIAR; ++j) {
+            int thread_id_inner = omp_get_thread_num();
+            tab[thread_id_outer][thread_id_inner] += a[i][j];
+#pragma omp ordered
+            {
+                printf("\tCOL\tj: %d\tthread_id: %d\n", j, thread_id_inner);
+            }
+        }
+#pragma omp ordered
+        {
+            printf("ROW\ti: %d\tthread_id: %d\n", i, thread_id_outer);
+        }
+    }
+
+    suma_parallel = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            suma_parallel += tab[i][j];
+        }
+    }
+
+    omp_set_nested(0);
+
+    printf("Suma wyrazów tablicy równolegle (2D): %lf\n", suma_parallel);
+
+    //
+    // Dodatkowa tablica
+    //
+
+    const int threads_count = 5;
+    omp_set_num_threads(threads_count);
+    double additional_array[threads_count];
+
+#pragma omp parallel for default(none) shared(additional_array, a)
+    for (int i = 0; i < WYMIAR; ++i) {
+        int thread_id = omp_get_thread_num();
+        for (int j = 0; j < WYMIAR; ++j) {
+            additional_array[thread_id] += a[i][j];
+        }
+    }
+
+    suma_parallel = 0.0;
+    for (int i = 0; i < threads_count; ++i)
+        suma_parallel += additional_array[i];
+
+    printf("Suma wyrazów tablicy równolegle (dodatkowa tablica): %lf\n", suma_parallel);
+}
---
+++ Lab 9/openmp_petle_simple.c
@@ -0,0 +1,85 @@
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WYMIAR 18
+
+// export OMP_NUM_THREADS=4
+// omp_set_num_threads(4);
+// ... num_threads(4)
+
+int main() {
+    double a[WYMIAR];
+
+#pragma omp parallel for default(none) shared(a)
+    for (int i = 0; i < WYMIAR; i++)
+        a[i] = 1.02 * i;
+
+    //
+    // pętla sekwencyjna
+    //
+
+    double suma = 0.0;
+    for (int i = 0; i < WYMIAR; i++) {
+        suma += a[i];
+    }
+
+    printf("Suma wyrazów tablicy: %lf\n", suma);
+
+    //
+    // rownolegle (static)
+    //
+
+    printf("\nSTATIC\n");
+
+    double suma_parallel = 0.0;
+#pragma omp parallel for default(none) shared(a) reduction(+ : suma_parallel) schedule(static) ordered
+    for (int i = 0; i < WYMIAR; i++) {
+        int id_w = omp_get_thread_num();
+        suma_parallel += a[i];
+#pragma omp ordered
+        printf("a[%2d]->W_%1d  \n", i, id_w);
+    }
+
+    printf("\nSuma wyrazów tablicy równolegle (z klauzulą - (static)): %lf\n", suma_parallel);
+
+    //
+    // rownolegle (static, 3)
+    //
+
+    printf("\nSTATIC 3\n");
+
+#pragma omp parallel for default(none) shared(a) reduction(+ : suma_parallel) schedule(static, 3) ordered
+    for (int i = 0; i < WYMIAR; i++) {
+        int id_w = omp_get_thread_num();
+#pragma omp ordered
+        printf("a[%2d]->W_%1d  \n", i, id_w);
+    }
+
+    //
+    // rownolegle (dynamic)
+    //
+
+    printf("\nDYNAMIC\n");
+
+#pragma omp parallel for default(none) shared(a) reduction(+ : suma_parallel) schedule(dynamic) ordered
+    for (int i = 0; i < WYMIAR; i++) {
+        int id_w = omp_get_thread_num();
+#pragma omp ordered
+        printf("a[%2d]->W_%1d  \n", i, id_w);
+    }
+
+    //
+    // rownolegle (dynamic, 3)
+    //
+
+    printf("\nDYNAMIC 3\n");
+
+#pragma omp parallel for default(none) shared(a) reduction(+ : suma_parallel) schedule(dynamic, 3) ordered
+    for (int i = 0; i < WYMIAR; i++) {
+        int id_w = omp_get_thread_num();
+#pragma omp ordered
+        printf("a[%2d]->W_%1d  \n", i, id_w);
+    }
+}
