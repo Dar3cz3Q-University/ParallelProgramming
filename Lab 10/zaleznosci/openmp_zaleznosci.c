0ccdec002e6907c353d7a0afd3a8f545fa7df7a0
Dariusz Homa <45432804+Dar3cz3Q@users.noreply.github.com>
2024-12-13 12:39:23 +0100

    Lab 10 (#10)
    * feat: lab 5 almost done
    
    * fix: edit comments

---
+++ Lab 10/zaleznosci/openmp_zaleznosci.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 1000000
+
+int main() {
+
+    int i;
+    double *A = malloc((N + 2) * sizeof(double));
+    double *B = malloc((N + 2) * sizeof(double));
+    double suma;
+
+    for (i = 0; i < N + 2; i++)
+        A[i] = (double)i / N;
+    for (i = 0; i < N + 2; i++)
+        B[i] = 1.0 - (double)i / N;
+
+    double t1 = omp_get_wtime();
+    for (i = 0; i < N; i++) {
+        A[i] += A[i + 2] + sin(B[i]);
+    }
+    t1 = omp_get_wtime() - t1;
+
+    suma = 0.0;
+    for (i = 0; i < N + 2; i++)
+        suma += A[i];
+    printf("suma %lf, czas obliczen %lf\n", suma, t1);
+
+    for (i = 0; i < N + 2; i++)
+        A[i] = (double)i / N;
+    for (i = 0; i < N + 2; i++)
+        B[i] = 1.0 - (double)i / N;
+
+    t1 = omp_get_wtime();
+
+    // wersja równoległa
+
+    t1 = omp_get_wtime() - t1;
+
+    suma = 0.0;
+    for (i = 0; i < N + 2; i++)
+        suma += A[i];
+    printf("suma %lf, czas obliczen rownoleglych %lf\n", suma, t1);
+}
---
+++ Lab 10/zmienne/openmp_watki_zmienne.c
@@ -0,0 +1,90 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+#ifdef _OPENMP
+    printf("\nKompilator rozpoznaje dyrektywy OpenMP\n");
+#endif
+
+    omp_set_num_threads(5);
+
+    int liczba_watkow;
+
+    int a_shared = 1;
+    int b_private = 2;
+    int c_firstprivate = 3;
+    int e_atomic = 5;
+
+    int static f_threadprivate = 0;
+#pragma omp threadprivate(f_threadprivate)
+
+    printf("przed wejsciem do obszaru rownoleglego -  nr_threads %d, thread ID %d\n",
+           omp_get_num_threads(), omp_get_thread_num());
+    printf("\ta_shared \t= %d\n", a_shared);
+    printf("\tb_private \t= %d\n", b_private);
+    printf("\tc_firstprivate \t= %d\n", c_firstprivate);
+    printf("\te_atomic \t= %d\n", e_atomic);
+    printf("\tf_threadprivate = %d\n", f_threadprivate);
+
+#pragma omp parallel default(none) shared(a_shared, e_atomic) private(b_private) firstprivate(c_firstprivate)
+    {
+        int i;
+        int d_local_private;
+        d_local_private = a_shared + c_firstprivate; // WAR [40 linia zmieni nam a_shared po zapisaniu d_local_private]
+
+#pragma omp barrier
+
+#pragma omp critical
+        {
+            for (i = 0; i < 10; i++) {
+                a_shared++; // RAW, WAW, WAR
+            }
+        }
+
+        for (i = 0; i < 10; i++) {
+            c_firstprivate += omp_get_thread_num();
+        }
+
+        for (i = 0; i < 10; i++) {
+#pragma omp atomic
+            e_atomic += omp_get_thread_num(); // RAW, WAW, WAR
+        }
+
+        f_threadprivate = omp_get_thread_num();
+
+#pragma omp barrier
+
+#pragma omp critical
+        {
+
+            printf("\nw obszarze równoległym: aktualna liczba watkow %d, moj ID %d\n",
+                   omp_get_num_threads(), omp_get_thread_num());
+
+            printf("\ta_shared \t= %d\n", a_shared);
+            printf("\tb_private \t= %d\n", b_private);
+            printf("\tc_firstprivate \t= %d\n", c_firstprivate);
+            printf("\td_local_private = %d\n", d_local_private);
+            printf("\te_atomic \t= %d\n", e_atomic);
+            printf("\td_local_private \t= %d\n", d_local_private);
+        }
+    }
+
+#pragma omp parallel default(none)
+    {
+#pragma omp critical
+        {
+            printf("\nw obszarze równoległym (drugim): aktualna liczba watkow %d, moj ID %d\n",
+                   omp_get_num_threads(), omp_get_thread_num());
+            printf("\tf_threadprivate = %d\n", f_threadprivate);
+        }
+    }
+
+    printf("\npo zakonczeniu obszaru rownoleglego:\n");
+    printf("\ta_shared \t= %d\n", a_shared);
+    printf("\tb_private \t= %d\n", b_private);
+    printf("\tc_firstprivate \t= %d\n", c_firstprivate);
+    printf("\te_atomic \t= %d\n", e_atomic);
+    printf("\tf_threadprivate = %d\n", f_threadprivate);
+}
